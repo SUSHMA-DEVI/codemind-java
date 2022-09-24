1c6d7603a053022be7f8fea72ec5e80197ad6047
SUSHMA-DEVI <96769402+SUSHMA-DEVI@users.noreply.github.com>
2022-09-24 19:35:04 +0530

    import java.util.Scanner; class Patt {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=1;i<=n;i++)         {             for(int j=1;j<=n;j++)             {                 if(j==1 || i==j || i==n)                 {                     System.out.print("*");                 }                 else                 {                     System.out.print(" ");                 }             }             System.out.print(" ");         }     } }

---
+++ print_the_following_pattern.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Patt
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(j==1 || i==j || i==n)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("
+");
+        }
+    }
+}
