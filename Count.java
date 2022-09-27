12b6156a3c1c8475922deb1fff0ae4d8e05133ad
SUSHMA-DEVI <96769402+SUSHMA-DEVI@users.noreply.github.com>
2022-09-27 21:04:39 +0530

    import java.util.Scanner; class Count {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] a=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         int c=0,c1=0;         for(int i=0;i<n;i++)         {             if(a[i]%2==0)             {                 c++;             }             else             {                 c1++;             }         }             System.out.format("%d %d",c,c1);
        }
    }

---
+++ Count.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Count
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int c=0,c1=0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]%2==0)
+            {
+                c++;
+            }
+            else
+            {
+                c1++;
+            }
+        }
+            System.out.format("%d %d",c,c1);
+            
+    }
+}
