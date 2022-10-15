6e578d0f3f9b1f90fa5f69f5bf924e73804631b4
SUSHMA-DEVI <96769402+SUSHMA-DEVI@users.noreply.github.com>
2022-10-15 21:04:21 +0530

    import java.util.*; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         int v=0,c=0,sp=0,d=0;         for(int i=0;i<s.length();i++)         {             if(s.charAt(i)=='a' || s.charAt(i)=='e'|| s.charAt(i)=='i'|| s.charAt(i)=='o' || s.charAt(i)=='u' || s.charAt(i)=='A' || s.charAt(i)=='E'|| s.charAt(i)=='I'|| s.charAt(i)=='O' || s.charAt(i)=='U')             {              v+=1;             }             else if(s.charAt(i)>='0' && s.charAt(i)<='9')             {                 d+=1;             }             else if(s.charAt(i)==' ')             {                 sp+=1;             }             else             {                 c+=1;             }         }         System.out.println("Vowels: "+v);         System.out.println("Consonants: "+c);         System.out.println("Digits: "+d);         System.out.println("White spaces: "+sp);     } }

---
+++ Count_no._of_vowels_,consonants_,digits_and_spaces.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int v=0,c=0,sp=0,d=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='a' || s.charAt(i)=='e'|| s.charAt(i)=='i'|| s.charAt(i)=='o' || s.charAt(i)=='u' || s.charAt(i)=='A' || s.charAt(i)=='E'|| s.charAt(i)=='I'|| s.charAt(i)=='O' || s.charAt(i)=='U')
+            {
+             v+=1;   
+            }
+            else if(s.charAt(i)>='0' && s.charAt(i)<='9')
+            {
+                d+=1;
+            }
+            else if(s.charAt(i)==' ')
+            {
+                sp+=1;
+            }
+            else
+            {
+                c+=1;
+            }
+        }
+        System.out.println("Vowels: "+v);
+        System.out.println("Consonants: "+c);
+        System.out.println("Digits: "+d);
+        System.out.println("White spaces: "+sp);
+    }
+}
