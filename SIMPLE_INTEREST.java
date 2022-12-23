5e1861a5ac9567c9e3e56ff5760116bf2c1a3478
SUSHMA-DEVI <96769402+SUSHMA-DEVI@users.noreply.github.com>
2022-12-23 14:29:08 +0530

    import java.util.*; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int p,t,r;         p=sc.nextInt();         t=sc.nextInt();         r=sc.nextInt();         int s;         s=(p*t*r)/100;         System.out.println(s);     } }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int p,t,r;
+        p=sc.nextInt();
+        t=sc.nextInt();
+        r=sc.nextInt();
+        int s;
+        s=(p*t*r)/100;
+        System.out.println(s);
+    }
+}
