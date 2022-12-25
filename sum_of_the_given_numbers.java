aae41bb08e148bcaedceb79973132fe25868e41b
SUSHMA-DEVI <96769402+SUSHMA-DEVI@users.noreply.github.com>
2022-12-25 17:14:49 +0530

    import java.util.*; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int c;         for(int i=0;i<n;i++)         {             int a=sc.nextInt(); int b=sc.nextInt();             c=a+b;             System.out.println(c);         }     } }

---
+++ sum_of_the_given_numbers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int c;
+        for(int i=0;i<n;i++)
+        {
+            int a=sc.nextInt(); int b=sc.nextInt();
+            c=a+b;
+            System.out.println(c);
+        }
+    }
+}
